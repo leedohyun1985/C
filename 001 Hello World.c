4e62f8a911a7d657f18fa16514286caf8b35a6fb
leedohyun1985 <leedohyun1985@gmail.com>
2021-01-31 12:59:57 +0900

    1강 Hello World
    20210131 이도현

---
+++ 001 Hello World.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+// Standard Input/Output library (표준입출력 라이브러리)
+// 제공함수는 https://ko.wikipedia.org/wiki/Stdio.h  에서 확인할 수 있음
+// h라는 확장자는 header file 이라는 뜻이다. 
+// https://www.tutorialspoint.com/cprogramming/c_header_files.htm 참조 
+int main(void)
+{
+	printf("Hello, World!");
+	return 0;
+}

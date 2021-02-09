a40c12dd462e0f30acab465f049324e30b16034c
leedohyun1985 <leedohyun1985@gmail.com>
2021-02-09 20:00:23 +0900

    20강 동적 메모리의 활용
    20210209 이도현

---
+++ 020 Usage of Memory Management 01 구조체 동적 메모리 할당.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Book
+{
+	int number;
+	char title[100];	
+};
+
+void showBook(struct Book *p, int n)
+{
+	int i;
+	for(i = 0 ; i < n ;i++)
+	{
+		printf("번호 %d : %s", (p+i)-> number, (p+i)-> title);
+		printf("\n");
+	}
+}
+
+int main(void)
+{
+	struct Book *p;
+	p = (struct Book *)malloc(2 * sizeof(struct Book));
+	if(NULL == p)
+	{
+		printf("동적 메모리 할당 실패");
+		exit(1); 
+	}
+	
+	p->number = 1;
+	strcpy( p -> title, "C Programming");
+	
+	(p + 1) -> number = 2;
+	strcpy( ( p + 1 ) -> title, "Data Structure");
+	
+	showBook(p, 2);
+	free(p);
+	return 0;
+}
